167b895f6b7faf86831dc71a601b874b89f17649
Steven Ong <116267297+stevenongxd@users.noreply.github.com>
2023-08-01 15:16:46 +0700

    Delete Version 3 directory

--- Version 3/script.js
+++
@@ -1,7 +0,0 @@
-const burgerMenu = document.querySelector('.burger-menu input')
-const nav= document.querySelector('#list-container');
-
-
-burgerMenu.addEventListener('click',function(){
-    nav.classList.toggle('slide');
-    });
